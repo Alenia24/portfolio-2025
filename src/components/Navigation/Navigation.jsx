8c187a475beb3cbe1869858fec04ebe435f63c57
Alenia24 <alenia.allenn@gmail.com>
2025-05-24 15:48:13 -0400

    Create navbar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 export default function Navigation() {
   return (
-    <div>Navigation</div>
-  )
+    <Navbar expand="lg" className="bg-body-transparent">
+      <Container>
+        <Navbar.Link as={Link} to={"/"}>A</Navbar.Link>
+        <Navbar.Link as={Link} to={"/"}>A</Navbar.Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+            <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
+            <Nav.Link as={Link} to={"/projects"}>Projects</Nav.Link>
+            <Nav.Link as={Link} to={"/contact"}>Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
