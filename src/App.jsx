4ad8323d240de2048a19eb07f33905b3ee3bd881
Alenia24 <alenia.allenn@gmail.com>
2025-05-24 15:29:27 -0400

    Import routes and set up route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <Routes>
+      <Route path="/" />
+      <Route path="/about" />
+      <Route path="/projects" />
+      <Route path="/contact" />
+    </Routes>
+  );
 }
 
 export default App
