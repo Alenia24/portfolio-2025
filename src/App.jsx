f75ea0b18246441dc10b9feafd4be4307cea08f7
Alenia24 <alenia.allenn@gmail.com>
2025-05-24 15:40:23 -0400

    Import pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
+
+// Import pages
+import Homepage from "./pages/Homepage/Homepage.jsx"
+import About from "./pages/About/About.jsx"
+import Projects from "./pages/Project/Project.jsx"
+import Contact from "./pages/Contact/Contact.jsx";
+
 import './App.css'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" />
-      <Route path="/about" />
-      <Route path="/projects" />
-      <Route path="/contact" />
+      <Route path="/" element={<Homepage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 }
