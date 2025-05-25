1ca6793c4d47883f4766f78e59d6596c3744706e
Alenia24 <alenia.allenn@gmail.com>
2025-05-24 20:39:54 -0400

    Create Homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,25 @@
 import Navigation from "../../components/Navigation/Navigation.jsx";
 
+import photo from "../../../src/assets/photo2.png"
+import "./Homepage.css"
+
 export default function Homepage() {
   return (
     <div>
       <Navigation />
+      <div className="title-container">
+        <div className="title">
+          <h2>Hello I'm&nbsp; 
+            <span className="typing-container">
+              <span className="title-highlight">Alenia,</span>
+            </span>
+          </h2> 
+          <h2 className="title-job">Full Stack Developer</h2>
+        </div>
+        <div className="image-container">
+          <img className="img-fluid" src={photo} alt="Photo of Alenia" width={230} />
+        </div>
+      </div>      
     </div>
   )
 }
