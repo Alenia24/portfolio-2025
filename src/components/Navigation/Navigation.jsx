1fd49495a02095a358a8c3bcc1103883913a727d
Alenia24 <alenia.allenn@gmail.com>
2025-05-24 21:08:28 -0400

    Remove border around nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -17,14 +17,12 @@ export default function Navigation() {
         </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <div className="nav-links ms-auto">
-          <Nav>
+          <Nav className="ms-auto nav-link">
             <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
             <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
             <Nav.Link as={Link} to={"/projects"}>Projects</Nav.Link>
             <Nav.Link as={Link} to={"/contact"}>Contact</Nav.Link>
           </Nav>
-          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
