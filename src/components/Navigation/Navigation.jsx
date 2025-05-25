f2cfe3ccc20a6bd62600f63bf511a9ae7a119ed9
Alenia24 <alenia.allenn@gmail.com>
2025-05-24 21:04:12 -0400

    Update css

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -17,12 +17,14 @@ export default function Navigation() {
         </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto nav-link">
+          <div className="nav-links ms-auto">
+          <Nav>
             <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
             <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
             <Nav.Link as={Link} to={"/projects"}>Projects</Nav.Link>
             <Nav.Link as={Link} to={"/contact"}>Contact</Nav.Link>
           </Nav>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
